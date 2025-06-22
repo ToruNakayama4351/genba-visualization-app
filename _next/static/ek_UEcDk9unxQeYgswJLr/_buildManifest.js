395e2f3b05a261e1823267dfb64f702869228544
ToruNakayama4351 <ToruNakayama4351@users.noreply.github.com>
2025-06-22 12:35:00 +0000

    deploy: 184410ffcb0a768ff5c2a723a9ecd897790ab02d

--- _next/static/ek_UEcDk9unxQeYgswJLr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/119-50bee947bd12e341.js","static/chunks/pages/index-da469bf93e2d7e74.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
